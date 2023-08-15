0d1aa62b32ec34199f41a12eb7c8618cfab069a7
VishalVermaa01 <vermavishal6020@gmail.com>
2023-08-15 20:56:05 +0530

    voting function for stakeholders using bool input

--- Contracts/Mainproperty.sol
+++ Contracts/Mainproperty.sol
@@ -40,8 +40,12 @@ mapping (address => PropertyFinances ) public PropertykiFinances ;
 mapping (address => PropertyDetails ) public PropertykiDetails ;
 
 
-
-constructor() ERC20("RealEstateToken", "RET") {}
+// adding parameters in constructor for voting function also
+constructor(string memory _decision) ERC20("RealEstateToken", "RET") {
+  owner = msg.sender;
+  decision = _decision;
+  isVotingOpen = true;
+}
 
  function DecideTokenprice(address _property)public {
   PropertyFinances storage property = PropertykiFinances[_property];
@@ -90,5 +94,71 @@ function DecideNetRentmonth(address _property) public {
     PropertyFinances storage property = PropertykiFinances[_property];
     _mint(_property ,property.NetRentmonth );
   }
+
+
+
+    //code for voting for the stakeholders
+    //address public owner;
+    string public decision;
+    uint public yesVotes;
+    uint public noVotes;
+    bool public isVotingOpen;
+
+
+    mapping(address => uint) public stakeHolders;
+
+    mapping(address => bool) public hasVoted;
+
+    mapping(address => bool) public castedVote;
+
+    
+    //already used constructor 
+    /* constructor(string memory _decision){
+        owner = msg.sender;
+        decision = _decision;
+        isVotingOpen = true;
+    }*/
+
+    function setStakeHolders(address _address, uint _stakeNo) public {
+        require(msg.sender == owner,"You are not the owner");
+        stakeHolders[_address] = _stakeNo;
+    }
+
+    function vote(bool _supportDecision) public {
+
+        require(stakeHolders[msg.sender] >= 1, "You don't have enough stakes");
+        require(hasVoted[msg.sender] == false, "You have already voted");
+
+        hasVoted[msg.sender] = true;
+
+        if (_supportDecision) {
+            yesVotes++;
+        } else {
+            noVotes++;
+        }
+
+    }
+
+    function closeVoting() public {
+        require(msg.sender == owner);
+        isVotingOpen = false;
+    }
+
+    function getResult() external view returns (string memory) {
+        require(!isVotingOpen, "Voting is still open");
+        
+        if (yesVotes > noVotes) { 
+            return "Decision is approved";
+        } else if (noVotes > yesVotes) {
+            return "Decision is rejected";
+        } else {
+            return "Decision is tied";
+        }
+    }
+
+    function getVoteCount() external view returns (uint256 yes, uint256 no) {
+        return (yesVotes, noVotes);
+    }
+
 
 }
